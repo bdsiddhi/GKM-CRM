b06ba9f9c8be421408e337a32e57520a2820b82d
Siddhi Infosoft <business.siddhi@gmail.com>
2021-11-12 14:26:49 +0530

    Updated encoding to UTF-8 for webhook

--- home/bitrix/www/importdeals/settings.php
+++ home/bitrix/www/importdeals/settings.php
@@ -1,3 +1,3 @@
 <?php
 define('C_REST_WEB_HOOK_URL','https://crm.gk-werkzeugmaschinen.com/rest/1/wssa2ve8hx2x2jmo/');
-define('C_REST_CURRENT_ENCODING','windows-1251');
+define('C_REST_CURRENT_ENCODING','UTF-8');
